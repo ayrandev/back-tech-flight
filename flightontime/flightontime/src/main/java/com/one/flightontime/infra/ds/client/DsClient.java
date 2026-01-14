fc6d2be0721371b4d91d1be0c03e5a785783fcab
Ayran <ayranvieira29@gmail.com>
2026-01-14 16:15:09 -0300

    Fazendo ajustes para deploy

--- flightontime/flightontime/src/main/java/com/one/flightontime/infra/ds/client/DsClient.java
+++ flightontime/flightontime/src/main/java/com/one/flightontime/infra/ds/client/DsClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "dsClient", url = "${FASTAPI_URL}")
+@FeignClient(name = "dsClient", url = "https://data-tech-flight.onrender.com")
 public interface DsClient {
 
     @PostMapping("/predict")
