e6f1f7ba892a010e225c832a4841465cec0392eb
Ayran <ayranvieira29@gmail.com>
2026-01-15 22:18:58 -0300

    Corriginto erro na url do fast api

--- flightontime/flightontime/src/main/java/com/one/flightontime/infra/ds/client/DsClient.java
+++ flightontime/flightontime/src/main/java/com/one/flightontime/infra/ds/client/DsClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "dsClient", url = "data-tech-flight-production.up.railway.app")
+@FeignClient(name = "dsClient", url = "https://data-tech-flight-production.up.railway.app")
 public interface DsClient {
 
     @PostMapping("/predict")
